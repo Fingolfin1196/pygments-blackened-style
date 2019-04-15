eca5b093e3b088d39f6710a384e65756e26083a3
fingolfin <kbo15@tu-clausthal.de>
2019-04-15 21:47:34 +0200

    * Some improved formatting.

--- pygments_blackened/style.py
+++ pygments_blackened/style.py
@@ -8,7 +8,7 @@
 
 from pygments.style import Style
 from pygments.token import Keyword, Name, Comment, String, Error, \
-     Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
+    Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
 
 
 class BlackenedStyle(Style):
@@ -20,36 +20,36 @@ class BlackenedStyle(Style):
     default_style = ""
 
     styles = {
-        Whitespace:                 '#fff',#bbbbbb
+        Whitespace:                 '#fff',  # bbbbbb
 
         Comment:                    'italic',
         Comment.Preproc:            'bold italic',
-        #Comment.Special:            'italic #0000aa',
+        # Comment.Special:            'italic #0000aa',
 
-        Keyword:                    'bold',#0000aa
-        #Keyword.Type:               '#00aaaa',
+        Keyword:                    'bold',  # 0000aa
+        # Keyword.Type:               '#00aaaa',
 
         Operator.Word:              '#0000aa',
 
         Name.Builtin:               'bold',
-        #Name.Function:              '#00aa00',
+        # Name.Function:              '#00aa00',
         Name.Class:                 'underline #00aa00',
         Name.Namespace:             'underline #00aaaa',
-        #Name.Variable:              '#aa0000',
+        # Name.Variable:              '#aa0000',
         Name.Constant:              '#aa0000',
         Name.Entity:                'bold #800',
         Name.Attribute:             '#1e90ff',
         Name.Tag:                   'bold #1e90ff',
         Name.Decorator:             '#888888',
 
-        String:                     '#000',#aa5500
-        #String.Symbol:              '#0000aa',
-        #String.Regex:               '#009999',
+        String:                     '#000',  # aa5500
+        # String.Symbol:              '#0000aa',
+        # String.Regex:               '#009999',
 
-        Number:                     '#000',#009999
+        Number:                     '#000',  # 009999
 
-        Generic.Heading:            'bold',#000080
-        Generic.Subheading:         'bold',#800080
+        Generic.Heading:            'bold',  # 000080
+        Generic.Subheading:         'bold',  # 800080
         Generic.Deleted:            '#aa0000',
         Generic.Inserted:           '#00aa00',
         Generic.Error:              '#aa0000',
@@ -59,5 +59,5 @@ class BlackenedStyle(Style):
         Generic.Output:             '#888888',
         Generic.Traceback:          '#aa0000',
 
-        #Error:                      '#F00 bg:#FAA'
+        # Error:                      '#F00 bg:#FAA'
     }
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 """ 
 Pygments stuff for the style
-""" 
+"""
 from setuptools import setup, find_packages
 
-setup( 
-    name         = 'pygments-blackened',
-    version      = '1.0',
-    description  = __doc__,
-    author       = "Kurt Böhm",
-    install_requires = ['pygments'],
-    packages     = find_packages(),
-    entry_points = '''
+setup(
+    name='pygments-blackened',
+    version='1.0',
+    description=__doc__,
+    author="Kurt Böhm",
+    install_requires=['pygments'],
+    packages=find_packages(),
+    entry_points='''
     [pygments.styles]
     blackened = pygments_blackened.style:BlackenedStyle
     '''
